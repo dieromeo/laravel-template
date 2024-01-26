91ff6db5e02edaacae703b2665120999a13352bd
dieromeo <diegoromeo99@icloud.com>
2024-01-26 14:53:02 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+// scss
+import '~resources/scss/app.scss'
+
+import.meta.glob([
+    '../img/**'
+])
+
+// Import our custom CSS
+import '~resources/scss/app.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    <title>Template title</title>
+</head>
+
+<body>
+    <h1>title</h1>
+    <!-- <img src="{{ Vite::asset('resources/img/laravel-logo.webp') }}" alt=""> -->
+</body>
+
+</html>
